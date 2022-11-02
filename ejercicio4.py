12e1faa5c2e8e98dbcbc0b9f2e184de816ef4259
juan miguel <jlojimeagu1@educacion.navarra.es>
2022-11-02 10:51:42 +0100

    ejercicio4 completado

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,4 +1,5 @@
-n_loteria = []
-n_loteria.append(input("dime los numeros de loteria!!!:\n"))
-for numero in n_loteria:
-    print(min(numero))
+n_ganadoresloteria = []
+for numero in range(6):
+    n_ganadoresloteria.append(int(input("dime los numeros de loteria, pon uno por uno!!!:\n")))
+n_ganadoresloteria.sort()
+print("los ganadores son:", n_ganadoresloteria)
