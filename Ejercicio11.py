d0258b85c38e75cabb2558a83dc05840a7c1cdeb
juan miguel <jlojimeagu1@educacion.navarra.es>
2022-11-08 12:49:02 +0100

    ejercicio11 completado

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,19 +1,19 @@
 A = ((1, 2, 3),
      (4, 5, 6))
 B = ((-1, 0),
+     (0, 1),
      (1, 1))
 resultado = [[0, 0],
              [0, 0]]
-for A in range(len(A)):
-    for B in range(len(B[0])):
+for a in range(len(A)):
+    for b in range(len(B[0])):
         for num in range(len(B)):
-            resultado[A][B] += A[A][num] * B[num][B]
+            resultado[a][b] += A[a][num] * B[num][b]
 
-for A in range(len(resultado)):
-    resultado[A] = tuple(resultado[A])
+for a in range(len(resultado)):
+    resultado[a] = tuple(resultado[a])
 
 resultado = tuple(resultado)
 
-for A in range(len(resultado)):
-    print(resultado[A])
-
+for a in range(len(resultado)):
+    print(resultado[a])
